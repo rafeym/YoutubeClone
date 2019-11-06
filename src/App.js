d5649f65a017e157ec45e29ba9dc9b0d8c52063d
rafeym <mohammadrafey0@gmail.com>
2019-11-06 17:30:20 -0500

    Added grids from material-ui to format app layout for components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react'
 
+import { Grid } from '@material-ui/core'
+
+// Reference to axios instance
+import youtube from './api/youtube'
+
 class App extends React.Component {
   render() {
-    return <h1>Youtube Clone App</h1>
+    return (
+      <Grid justify='center' container spacing={16}>
+        <Grid item xs={12}>
+          <Grid container spacing={16}>
+            <Grid item xs={12}>
+              {/* SEARCH BAR */}
+            </Grid>
+            <Grid item xs={8}>
+              {/* VIDEO DETAILS */}
+            </Grid>
+            <Grid item xs={4}>
+              {/* VIDEO LIST */}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    )
   }
 }
 
