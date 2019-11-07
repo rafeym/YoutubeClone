d27b74656a733a56695574af4d66c71f88fdde5f
rafeym <mohammadrafey0@gmail.com>
2019-11-06 23:36:12 -0500

    Video item and video list component complete

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 import {Grid, Paper, Typography} from '@material-ui/core'
 
-const VideoItem = () => {
+const VideoItem = ({video, onVideoSelect}) => {
+
     return (
-        <h1>Video Item</h1>
+        <Grid item xs={12}>
+            <Paper style={{display: 'flex', alignItems: 'center', cursor:'pointer'}} onClick={() => onVideoSelect(video)} >
+                <img style={{marginRight: '20px'}} alt="thumbnail" src={video.snippet.thumbnails.medium.url}/>
+                <Typography variant="subtitle1"><bold>{video.snippet.title}</bold></Typography>
+            </Paper>
+        </Grid>
     )
 }
 
