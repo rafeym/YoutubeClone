a7ba6cf36964e611be33e1c51f26fd4747f72f55
rafeym <mohammadrafey0@gmail.com>
2019-11-06 20:50:23 -0500

    updated youtube.js api file

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,10 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3',
-    params: {
-        part: 'snippet',
-        maxResults: 5,
-        key: 'AIzaSyDaQJrwcnbRMVluOe0dRm13o4HtpkUuKXM'
-    }
+    baseURL: 'https://www.googleapis.com/youtube/v3'
 })
